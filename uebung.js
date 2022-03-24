c637fba2f9620aa65d6f674bf48e66b111b0bcee
mariuslaser <cvnb>
2022-03-24 14:04:19 +0100

    initial Commit

--- uebung.js
+++ uebung.js
@@ -1,4 +1,16 @@
+let wochentage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
 
 function ausgabe() {
-    
+
+    console.log(wochentage)
 }
+
+function renameFriday() {
+
+wochentage[4] = "nochNichtFreitag"
+
+    console.log(wochentage[4])
+
+}
+
+let Monate = ["Januar", "Februar", "März", "April","Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
